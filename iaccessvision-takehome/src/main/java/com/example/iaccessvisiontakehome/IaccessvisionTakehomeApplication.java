c610bbd3b5a74784a7689b3ea7bd9414a55b4d0f
thomas-nii <35361333+thomas-nii@users.noreply.github.com>
2023-09-24 15:50:53 +0800

    Add files via upload

---
+++ iaccessvision-takehome/src/main/java/com/example/iaccessvisiontakehome/IaccessvisionTakehomeApplication.java
@@ -0,0 +1,13 @@
+package com.example.iaccessvisiontakehome;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IaccessvisionTakehomeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IaccessvisionTakehomeApplication.class, args);
+	}
+
+}
---
+++ iaccessvision-takehome/src/test/java/com/example/iaccessvisiontakehome/IaccessvisionTakehomeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.iaccessvisiontakehome;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IaccessvisionTakehomeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
